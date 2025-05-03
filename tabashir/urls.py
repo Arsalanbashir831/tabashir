cfeedfcd1dd2bc632fbc6cf8160bdc6baf8eca18
Arsalan831 <arsalanbashir831@gmail.com>
2025-05-03 17:46:33 +0500

    add token verification

--- tabashir/urls.py
+++ tabashir/urls.py
@@ -17,18 +17,21 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView,TokenVerifyView
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
+   
     path('admin/', admin.site.urls),
     path('user/', include('user.urls')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
     path('job/', include('job.urls')),
     path('resumes/', include('resume.urls')),
     path('ai/', include('ai_generator.urls')),
+    
 ]
 
 
